90f4f946c08a850b1f43424d37a69fc61966e5eb
Beqa Gozalishvili <beqaprogger@gmail.com>
2022-01-02 19:23:56 +0400

    added more notification IDs to be configured

--- globalPlugins/msedge/__init__.py
+++ globalPlugins/msedge/__init__.py
@@ -6,6 +6,9 @@ import wx
 confspec = {
     "PageLoading": "boolean(default=false)",
     "RefreshingPage": "boolean(default=false)",
+    "ClosingTab": "boolean(default=false)",
+    "OpeningNewTab": "boolean(default=false)",
+    "OpeningWindow": "boolean(default=false)",
 }
 config.conf.spec["MSEdgeDiscardAnnouncements"] = confspec
 
@@ -31,7 +34,16 @@ class MSEdgeDiscardAnnouncementsPanel(gui.settingsDialogs.SettingsPanel):
         self.reportPageLoad.SetValue(self.config["PageLoading"])
         self.reportPageRefresh = helper.addItem(wx.CheckBox(self, wx.ID_ANY, label=_("Announce page refresh")))
         self.reportPageRefresh.SetValue(self.config["RefreshingPage"])
+        self.reportCloseTab = helper.addItem(wx.CheckBox(self, wx.ID_ANY, label=_("Announce closing of tab")))
+        self.reportCloseTab.SetValue(self.config["ClosingTab"])
+        self.reportOpenNewTab = helper.addItem(wx.CheckBox(self, wx.ID_ANY, label=_("Announce Opening of new tab")))
+        self.reportOpenNewTab.SetValue(self.config["OpeningNewTab"])
+        self.reportOpenWindow = helper.addItem(wx.CheckBox(self, wx.ID_ANY, label=_("Announce window opening")))
+        self.reportOpenWindow.SetValue(self.config["OpeningWindow"])
 
     def onSave(self):
         self.config["PageLoading"] = self.reportPageLoad.GetValue()
         self.config["RefreshingPage"] = self.reportPageRefresh.GetValue()
+        self.config["ClosingTab"] = self.reportCloseTab.GetValue()
+        self.config["OpeningNewTab"] = self.reportOpenNewTab.GetValue()
+        self.config["OpeningWindow"] = self.reportOpenNewWindow.GetValue()
