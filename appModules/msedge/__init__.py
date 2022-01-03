921a59ffe7277d188631b4ef6b99df5bb9c2b462
Beqa Gozalishvili <beqaprogger@gmail.com>
2022-01-03 09:08:30 +0400

    Optimize getting notification IDs from config a bit.

--- appModules/msedge/__init__.py
+++ appModules/msedge/__init__.py
@@ -2,13 +2,15 @@ import appModuleHandler
 import config
 
 class AppModule(appModuleHandler.AppModule):
+    activityIDs = []
 
-    def getActivityIDSFromConfig(self):
+    def getActivityIDsFromConfig(self):
         edgeConf = config.conf["MSEdgeDiscardAnnouncements"]
-        activityIDS = [i[0] for i in edgeConf.items() if type(i[1]) == bool and i[1] == False]
-        return activityIDS
+        self.activityIDs = [i[0] for i in edgeConf.items() if type(i[1]) == bool and i[1] == False]
+
+    def event_appModule_gainFocus(self):
+        self.getActivityIDsFromConfig()
 
     def event_UIA_notification(self, obj, nextHandler, activityId, **kwargs):
-        activityIDS = self.getActivityIDSFromConfig()
-        if activityId in activityIDS: return
+        if activityId in self.activityIDs: return
         nextHandler()
