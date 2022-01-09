ec132a8b67a72716022d7608c34e76f04979c448
Beqa Gozalishvili <beqaprogger@gmail.com>
2022-01-09 09:54:43 +0400

    added some more notification IDs  to be suppressed

--- addon/appModules/msedge/settings.py
+++ addon/appModules/msedge/settings.py
@@ -14,10 +14,13 @@ settingItems = [
     Settings("ClosingTab", _("Announce closing of tab"), "boolean(default=false)"),
     Settings("OpeningNewTab", _("Announce Opening of new tab"), "boolean(default=false)"),
     Settings("OpeningWindow", _("Announce window opening"), "boolean(default=false)"),
+    Settings("OpeningInPrivateWindow", _("Announce opening of inprivate window"), "boolean(default=false)"),
     Settings("HubDownloadsNewDownload", _("Announce starting file download"), "boolean(default=true)"),
     Settings("HubDownloadsCompleteState", _("Announce download completion"), "boolean(default=true)"),
     Settings("GoingBack", _("Announce navigating back"), "boolean(default=false)"),
     Settings("ToolbarButtonRemoved", _("Announce removing toolbar buttons"), "boolean(default=false)"),
     Settings("SearchMode", _("Announce of search mode"), "boolean(default=false)"),
-    Settings("SearchModeAvailable", _("Announce availability of search mode"), "boolean(default=false)")
+    Settings("SearchModeAvailable", _("Announce availability of search mode"), "boolean(default=false)"),
+    Settings("NotificationAppear", _("Announce appearing of notifications"), "boolean(default=false)"),
+    Settings("HubDownloadsIndeterminateProgressState", _("Announce indeterminate progress state of current download"), "boolean(default=false)")
 ]
